4afd9ecea0c42b2ef545873ef4c689da23c931c6
Hussien12Mostafa <85521642+Hussien12Mostafa@users.noreply.github.com>
2023-03-29 20:06:54 +0200

    last version

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <unistd.h>
+
 using namespace std;
 
 const int gridSize = 100;
 void printGrid(bool gridOne[gridSize + 1][gridSize + 1]);
-void determineState(bool gridOne[gridSize + 1][gridSize + 1]);
-void clearScreen(void);
+void deterministic(bool gridOne[gridSize + 1][gridSize + 1]);
+
 
 int main()
 {
 
   bool gridOne[gridSize + 1][gridSize + 1] = {};
 
-  for (int a = 1; a < gridSize; a++)
+  for (int row = 1; row < gridSize; row++)
   {
-    for (int b = 1; b < gridSize; b++)
-    {
-      if (a == 10)
-        gridOne[a][b] = true;
+    for (int col = 1; col < gridSize; col++)
+    {  
+      if(row%4==0 )
+        gridOne[row][col] = true;
     }
   }
 
@@ -25,56 +26,46 @@ int main()
   for (int i =0;i<100;i++)
   {
     printGrid(gridOne);
-    determineState(gridOne);
+    deterministic(gridOne);
     usleep(2000000);
   }
 }
 
 void printGrid(bool gridOne[gridSize + 1][gridSize + 1])
 {
-  for (int a = 1; a < gridSize; a++)
+  for (int row = 1; row < gridSize; row++)
   {
-    for (int b = 1; b < gridSize; b++)
+    for (int col = 1; col < gridSize; col++)
     {
-      if (gridOne[a][b] == true)
+      if (gridOne[row][col] == true)
         cout << " O ";
       else
         cout << " . ";
-      if (b == gridSize - 1)
+      if (col == gridSize - 1)
         cout << endl;
     }
   }
   cout << "--------------------------------------------------------------------------------------------------------------------------------" << endl;
 }
 
-void compareGrid(bool gridOne[gridSize + 1][gridSize + 1], bool gridTwo[gridSize + 1][gridSize + 1])
-{
-  for (int a = 0; a < gridSize; a++)
-  {
-    for (int b = 0; b < gridSize; b++)
-    {
-      gridTwo[a][b] = gridOne[a][b];
-    }
-  }
-}
-
-void determineState(bool gridOne[gridSize + 1][gridSize + 1])
+void deterministic(bool gridOne[gridSize + 1][gridSize + 1])
 {
   bool gridTwo[gridSize + 1][gridSize + 1] = {};
-  compareGrid(gridOne, gridTwo);
 
-  for (int a = 1; a < gridSize; a++)
+  std::copy(&gridOne[0][0], &gridOne[0][0]+(gridSize+1)*(gridSize+1),&gridTwo[0][0]);
+
+  for (int row = 1; row < gridSize; row++)
   {
-    for (int b = 1; b < gridSize; b++)
+    for (int col = 1; col < gridSize; col++)
     {
       int alive = 0;
-      for (int c = -1; c < 2; c++)
+      for (int r = -1; r < 2; r++)
       {
-        for (int d = -1; d < 2; d++)
+        for (int c = -1; c < 2; c++)
         {
-          if (!(c == 0 && d == 0))
+          if (!(r == 0 && c == 0))
           {
-            if (gridTwo[a + c][b + d])
+            if (gridTwo[row + r][col + c])
             {
               ++alive;
             }
@@ -83,15 +74,15 @@ void determineState(bool gridOne[gridSize + 1][gridSize + 1])
       }
       if (alive < 2)
       {
-        gridOne[a][b] = false;
+        gridOne[row][col] = false;
       }
       else if (alive == 3)
       {
-        gridOne[a][b] = true;
+        gridOne[row][col] = true;
       }
       else if (alive > 3)
       {
-        gridOne[a][b] = false;
+        gridOne[row][col] = false;
       }
     }
   }
